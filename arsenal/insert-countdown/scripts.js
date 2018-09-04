3b07c303702832c697e8a84659eae302c52eb80e
Adam Pengh <apengh@monetate.com>
2018-09-04 12:09:45 -0400

    arsenel

---
+++ arsenal/insert-countdown/scripts.js
@@ -0,0 +1,4 @@
+/* Required for Mobile - Tells site code about the new elements to hook into existing animations */
+jQuery(document).ready(function() {
+  window.$elem = $(".bottom-header-wrapper .content"), window.l = $elem.length, window.i = 0;
+});
