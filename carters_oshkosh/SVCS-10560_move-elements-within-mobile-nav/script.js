40c69ce0133ae41cd10a1fe25804c3080a331039
Adam Pengh <apengh@monetate.com>
2018-09-06 15:23:12 -0400

    9/6

--- carters_oshkosh/SVCS-10560_move-elements-within-mobile-nav/script.js
+++ carters_oshkosh/SVCS-10560_move-elements-within-mobile-nav/script.js
@@ -1,6 +1,10 @@
-var specialOffers = $('#navigation > nav > div.items-navigation > ul > li.baby-girl.topCat > div.subnav > div > div.column.promo');
-var categories = $('#navigation > nav > div.items-navigation > ul > li.baby-girl.topCat > div.subnav > div > div.column.first > ul > li.menuTitle');
+var specialOffers = $('#nav > li.baby-girl.topCat > div.subnav > div > div.column.promo');
+var categories = $('#nav > li.baby-girl.topCat > div.subnav > div > div.column.first > ul > li.menuTitle');
 
 $('div.column.promo ul').css('column-count', 'auto');
 
 categories.before(specialOffers);
+
+
+'#navigation > nav > div.items-navigation > ul.items-navigation-links > li.baby-girl > div.subnav > div.subnav-categories > div.column.promo'
+'#navigation > nav > div.items-navigation > ul.items-navigation-links > li.baby-girl > div.subnav > div.subnav-categories > div.column.first > ul > li.menuTitle'
