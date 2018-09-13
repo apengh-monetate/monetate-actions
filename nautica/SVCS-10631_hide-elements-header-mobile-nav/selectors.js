ce66fb426509a283a358682f4f7e9e6e1b152674
Adam Pengh <apengh@monetate.com>
2018-09-13 07:33:54 -0400

    9/13/2018

---
+++ nautica/SVCS-10631_hide-elements-header-mobile-nav/selectors.js
@@ -0,0 +1,11 @@
+// Gift Card
+'#pre-header ul.systemControls li:has(a[href*="/giftcard"]), #slideMenu li.link-bar-item.static-elem:has(a[href*="/giftcard"])'
+
+// Email Signup
+'#pre-header ul.systemControls li:has(a.emailSignup)'
+
+// Rewards
+'#pre-header ul.systemControls li:has(a[href*="/rewards"]), #slideMenu li.link-bar-item.static-elem:has(a[href*="/rewards"])'
+
+// Live Chat
+'#livechat, #slideMenu li.link-bar-item.static-elem:has(a[href*="/livechat/"])'
---
+++ wrangler/SVCS-10632_hide-quantity-of-1/selector.js
@@ -0,0 +1,7 @@
+.mini-cart-item-qty[data-qty="1"],
+.checkout-summary-item-table-item .item-detail-info > div > div:contains(Quantity:1)
+
+
+
+.mini-cart-item-qty[data-qty="1"]
+.checkout-summary-item-table-item .item-detail-qty:contains(span.item-detail-qty-label[data-quantity="1"])
